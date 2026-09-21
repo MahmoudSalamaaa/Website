4f9362294f9c76dbbe99a11b67751df6b04929d4
Mahmoud Salama <89738312+MahmoudSalamaaa@users.noreply.github.com>
2026-09-21 20:05:46 +0300

    Route legacy CV actions to canonical CV center

--- mobile-rail-controls.js
+++ mobile-rail-controls.js
@@ -36,7 +36,7 @@
       ['GitHub', 'https://github.com/MahmoudSalamaaa'],
       ['LinkedIn', 'https://www.linkedin.com/in/mahmoud-salama-30249b34'],
       ['Email', 'mailto:ma7moud.salamaaa@gmail.com'],
-      ['Download CV', 'https://drive.google.com/file/d/1lZ6uz6gLUetG-oPLC4iRqOideR4-8lhz/view'],
+      ['Download CV', 'cv-hub.html'],
       ['Digital Business Card', 'card.html']
     ];
 

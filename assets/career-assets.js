da37a69599fd2df4a49eaadb857bfe7c7cdb8f87
Mahmoud Salama <89738312+MahmoudSalamaaa@users.noreply.github.com>
2026-09-22 09:01:59 +0300

    Add canonical career asset registry

---
+++ assets/career-assets.js
@@ -0,0 +1,10 @@
+window.MS_CAREER_ASSETS={
+updated:"2026-09-22",
+portfolio:"https://drive.google.com/file/d/18Vph6D9UY5lMV0rY1qBL_3mil2AjUFod/view?usp=drivesdk",
+roles:{
+cto:{label:"CTO / CDTO",cv:"https://drive.google.com/file/d/1qvbiDGPqw8eSqjIUO1M5S_2i65MZ2FCw/view?usp=drivesdk",cl:"https://drive.google.com/file/d/1epqrUQ45B8mvL7i4SA_gYk0selJorrt5/view?usp=drivesdk"},
+cio:{label:"CIO / IT Director",cv:"https://drive.google.com/file/d/1V6E-r5rjPPLZOuJC2wv5jcVVhxjkwZj3/view?usp=drivesdk",cl:"https://drive.google.com/file/d/1O_QnvCjitx0HMrZLyZNCsZKo8TrAVMMv/view?usp=drivesdk"},
+architecture:{label:"Enterprise Architecture",cv:"https://drive.google.com/file/d/1tcYdkwvDPup2Qj9xDSP5F-lZ1g1fh17z/view?usp=drivesdk",cl:"https://drive.google.com/file/d/1DJxp2CU_13CUAbye2x7FkjvCdKz4bAI1/view?usp=drivesdk"},
+program:{label:"Program Director / Head of Delivery",cv:"https://drive.google.com/file/d/1QbFyxAa9Nd6e-k_w8vHNoLRA_VGdPOz8/view?usp=drivesdk",cl:"https://drive.google.com/file/d/19H8zmAf58g72lrYABpOEHtKs0Ze6nNeG/view?usp=drivesdk"},
+engineering:{label:"Software Engineering & Applications",cv:"https://drive.google.com/file/d/13ixhbbcn7wel0Dqp5pueCBn92-MI-BLG/view?usp=drivesdk",cl:"https://drive.google.com/file/d/1rGYu58Gcj8TRLiUWt4t_tod_izay1o/view?usp=drivesdk"}
+}};

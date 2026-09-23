e841d5092346b626c9abd471630258eed2f1cb7d
Mahmoud Salama <89738312+MahmoudSalamaaa@users.noreply.github.com>
2026-09-23 21:15:29 +0300

    Remove superseded static career intelligence workspace

--- organizations/assets/search-worker.js
+++
@@ -1,1 +0,0 @@
-/* Search runs from a Blob worker embedded in app.js for file:// and hosted compatibility. */
--- organizations/start-local.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Opening Mahmoud Salama Career Intelligence on http://localhost:8080
-start "" http://localhost:8080/index.html
-python -m http.server 8080
-pause
